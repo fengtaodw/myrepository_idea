dd5254a76b4f6b3b23e85ae19e87f43be093ed54
lft <lft@itcast.cn>
2020-07-24 10:24:20 +0800

    第三次提交代码

--- HelloProject/src/itcast/HelloWorld.java
+++ HelloProject/src/itcast/HelloWorld.java
@@ -3,5 +3,6 @@ package itcast;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("Hello java");
     }
 }
