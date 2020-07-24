75c63c23c22ff8db88bd39e88ded472246c17f3c
fengtaodw <54674183+fengtaodw@users.noreply.github.com>
2020-07-24 10:49:49 +0800

    Update HelloWorld.java

--- HelloProject/src/itcast/HelloWorld.java
+++ HelloProject/src/itcast/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("Hello java");
+        System.out.println("Hello linux");
     }
 }
